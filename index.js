fdf6ab9a455d2c45d02953d1fc31f136754d18c0
Haruna Lawali <lawaliharuna943@gmail.com>
2025-10-18 11:39:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async scheduled(event, env, ctx) {
     const urls = [
-      "https://tauraronwasa.pages.dev/api/site6?key=@haruna66",
+      
       "https://bonus-gamma.vercel.app/api/monitorDeposits?key=@haruna66"
     ];
 
